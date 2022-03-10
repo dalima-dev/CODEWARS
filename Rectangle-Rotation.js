89f3fd3efd81cf3941af76a9f4a50c67d92996d0
dalima-dev <dalima.dev@gmail.com>
2022-03-10 15:29:03 -0300

    update

--- Rectangle-Rotation.js
+++ Rectangle-Rotation.js
@@ -1,17 +1,17 @@
 function rectangleRotation(a, b) {
-    let count = 0;
-    let exclude_y_half = 0;
-    let exclude_x_half = 0;
-    const end_y = b/2;
-    const end_x = a/2;
-    for(let y = 0; y <= end_y; y += Math.sqrt(2)) exclude_y_half++;
-    for(let x = 0; x <= end_x; x += Math.sqrt(2)) exclude_x_half++;
-    let i;
-    for(let y = 0, j = 0; y <= end_y; y += Math.sqrt(2)/2, j++){
-        if(j % 2 == 0) i = 0;
-        else
-        i = Math.sqrt(2)/2;
-        for(let x = i; x <= end_x; x += Math.sqrt(2)) count++;
-    }
-    return (count*2 - exclude_x_half)*2 - (exclude_y_half*2 - 1);
-}    
+  let count = 0;
+  let exclude_y_half = 0;
+  let exclude_x_half = 0;
+  const end_y = b / 2;
+  const end_x = a / 2;
+  for (let y = 0; y <= end_y; y += Math.sqrt(2)) exclude_y_half++;
+  for (let x = 0; x <= end_x; x += Math.sqrt(2)) exclude_x_half++;
+  let i;
+  for (let y = 0, j = 0; y <= end_y; y += Math.sqrt(2) / 2, j++) {
+    if (j % 2 == 0) i = 0;
+    else i = Math.sqrt(2) / 2;
+    for (let x = i; x <= end_x; x += Math.sqrt(2)) count++;
+  }
+  return (count * 2 - exclude_x_half) * 2 - (exclude_y_half * 2 - 1);
+}
+
---
+++ test.js
@@ -0,0 +1,13 @@
+class person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  get showName() {
+    return this.name;
+  }
+}
+
+const obj = new person("dani", 26);
+
+//editing code
